ff47870fde5946f6e884668106e270b2c47136a8
viviann224 <viviann224@gmail.com>
2021-01-13 21:16:52 -0600

    added virtual shower and paw it forward project

--- portfolio/assets/js/portfolio.js
+++ portfolio/assets/js/portfolio.js
@@ -1,3 +1,7 @@
 document.querySelector(".info").addEventListener('shown.bs.modal', function () {
   document.querySelector(".modal-dialog").focus()
 })
+
+// document.querySelector("#pawitModal").addEventListener('shown.bs.modal', function () {
+//   document.querySelector(".pawit").focus()
+// })
